7fcbd6a8ff2f0e9279961e346b86d5c247bec2a2
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-09 22:50:47 +0200

    fix: update models and link them to admin page

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Album(models.Model):
     name = models.CharField(max_length=150, blank=True, default='New Album')
     artist = models.ForeignKey(
-        'artists.Artist', on_delete=models.CASCADE, blank=True, null=True)
+        'artists.Artist', on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
     release_datetime = models.DateTimeField()
     cost = models.DecimalField(max_digits=6, decimal_places=2)
--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Artist(models.Model):
-    stage_name = models.CharField(max_length=150, primary_key=True)
+    stage_name = models.CharField(
+        max_length=150, primary_key=True)
     social_media_link = models.URLField(max_length=250, blank=True, default='')
 
     def __str__(self):
