96c12c7b014e420bea42e194677cbc0bdc3c2640
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-09 23:12:28 +0200

    feat: setup dotenv

--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3lz4!&do=o!7sn07(ev8d039j435isk0so%*ug3te^xou45c-!'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-3lz4!&do=o!7sn07(ev8d039j435isk0so%*ug3te^xou45c-!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG', '1')) == '1'
 
 ALLOWED_HOSTS = []
+
+if not DEBUG:
+    ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
 
 
 # Application definition
