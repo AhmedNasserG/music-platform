a81b56d18c25714a9e1b32289e94c428e67e26c7
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-09 19:54:35 +0200

    style: formate files using autopep8

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -4,10 +4,11 @@ from django.utils import timezone
 
 class Album(models.Model):
     name = models.CharField(max_length=150, blank=True, default='New Album')
-    artist = models.ForeignKey('artists.Artist', on_delete=models.CASCADE, blank=True, null=True)
+    artist = models.ForeignKey(
+        'artists.Artist', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(default=timezone.now)
     release_datetime = models.DateTimeField()
     cost = models.DecimalField(max_digits=6, decimal_places=2)
-    
+
     def __str__(self):
         return self.name
