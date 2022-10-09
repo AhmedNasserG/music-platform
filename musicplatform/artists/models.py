9710cda968f5d844ee0339e659f136d8f37f1509
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-09 18:30:50 +0200

    Add project init filese

---
+++ musicplatform/artists/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Artist(models.Model):
+    stage_name = models.CharField(max_length=150, primary_key=True)
+    social_media_link = models.URLField(max_length=250, blank=True, default='')
+
+    def __str__(self):
+        return self.stage_name
