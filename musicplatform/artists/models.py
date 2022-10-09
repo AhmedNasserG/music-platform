3b21ce1a8a243983c66b7811e545e41380eb43e5
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-10 00:19:38 +0200

    fix: update models

--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Artist(models.Model):
     stage_name = models.CharField(
-        max_length=150, primary_key=True)
+        max_length=150)
     social_media_link = models.URLField(max_length=250, blank=True, default='')
+
+    class Meta:
+        ordering = ['stage_name']
 
     def __str__(self):
         return self.stage_name
