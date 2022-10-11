252ce6c79e993341ac42923a6f9107a5724378fa
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-11 18:11:09 +0200

    feat: add reviewed_by_admin field to Album model

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -7,9 +7,10 @@ from artists.models import Artist
 class Album(models.Model):
     name = models.CharField(max_length=150, blank=True, default='New Album')
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
     release_datetime = models.DateTimeField()
     cost = models.DecimalField(max_digits=6, decimal_places=2)
+    reviewed_by_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
