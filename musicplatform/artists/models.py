55b613fd1221bdcdd20603c310ed9ebebe29f51e
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-11 16:08:38 +0200

    fix: update database models

--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -1,9 +1,9 @@
+from enum import unique
 from django.db import models
 
 
 class Artist(models.Model):
-    stage_name = models.CharField(
-        max_length=150)
+    stage_name = models.CharField(max_length=150, unique=True)
     social_media_link = models.URLField(max_length=250, blank=True, default='')
 
     class Meta:
