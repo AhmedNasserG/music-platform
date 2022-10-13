a3e3f63c3b72ef4fca9b2227cab2af12cdfa1adf
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-13 13:14:05 +0200

    feat: add create album form

--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
+
+from .forms import AlbumForm
 
 
 def index(request):
     return render(request, 'albums/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = AlbumForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/artists/')
+    else:
+        form = AlbumForm()
+    return render(request, 'albums/create.html', {'form': form})
