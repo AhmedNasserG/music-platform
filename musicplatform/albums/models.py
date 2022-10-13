d3eb73333cf05f57fd95ee792f1cf76875ada401
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-13 11:25:28 +0200

    refactor: use TimeStampedModel in Album model
    Instead of having an explicit `created_at` field in the `Album` model, inherit from `TimeStampedModel`

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.utils import timezone
+from model_utils.models import TimeStampedModel
 
 from artists.models import Artist
 
 
-class Album(models.Model):
+class Album(TimeStampedModel):
     name = models.CharField(max_length=150, blank=True, default='New Album')
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now, editable=False)
     release_datetime = models.DateTimeField()
     cost = models.DecimalField(max_digits=6, decimal_places=2)
     reviewed_by_admin = models.BooleanField(
