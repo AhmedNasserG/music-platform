0fa9cd667199dfb5287bbcb6a4104cb96efb08fa
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-17 17:10:16 +0200

    feat: setup imagekit

--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'artists.apps.ArtistsConfig',
     'albums.apps.AlbumsConfig',
+    'imagekit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- musicplatform/musicplatform/urls.py
+++ musicplatform/musicplatform/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('artists/', include('artists.urls')),
     path('albums/', include('albums.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
