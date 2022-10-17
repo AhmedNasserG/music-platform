f7873575d7ae11a6f3259dd3ad3c882232f2a26e
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-17 18:51:21 +0200

    fix: make admin add at least one song per album

--- musicplatform/albums/admin.py
+++ musicplatform/albums/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
+from .forms import AtLeastOneRequiredInlineFormSet
 from .models import Album, Song
 
-admin.site.register(Album)
+
+class SongInline(admin.TabularInline):
+    model = Song
+    formset = AtLeastOneRequiredInlineFormSet
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [SongInline, ]
+
+
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Song)
