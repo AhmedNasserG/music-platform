94e7df2b917446256b6b8cbdb8be41e3cf537009
AhmedNasserG <ahmednasser217217@gmail.com>
2022-10-30 15:17:06 +0200

    fix: users tests

--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -118,7 +118,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication', ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2,
-    'DEFAULT_FILTER_BACKENDS': 'django_filters.rest_framework.DjangoFilterBackend',
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 
